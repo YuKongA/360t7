80b7a1bcbe6cdab3f85b4211d51bbcb48a38d660
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-05-13 18:32:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Build OpenWrt for 360T7
---
+++ diy-part2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i "s/DISTRIB_TARGET=.*/DISTRIB_TARGET='mediatek/mt7981'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='%D(%C) By YuKongA'/g" package/base-files/files/etc/openwrt_release
